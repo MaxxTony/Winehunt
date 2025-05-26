9142d9aab308fca27c42f1283fc754fedbe01fd2
jailendra <jailendra.isb@gmail.com>
2025-05-26 13:07:28 +0530

    few more

--- src/screens/Profile/AddressList.js
+++ src/screens/Profile/AddressList.js
@@ -23,7 +23,7 @@ import DeleteModal from '../../Modal/DeleteModal';
 import AddressModal from '../../Modal/AddressModal';
 import {showSucess, showWarning} from '../../helper/Toastify';
 import Modal from 'react-native-modal';
-import {Dropdown} from 'react-native-element-dropdown';
+import {Dropdown, SelectCountry} from 'react-native-element-dropdown';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from '../../helper/Constant';
 import axios from 'axios';
@@ -49,6 +49,9 @@ const AddressList = () => {
   const [flat, setFlat] = useState('');
   const [area, setArea] = useState('');
   const [pincode, setPincode] = useState('');
+
+  const [selectedAddress, setSelectedAddress] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
 
   useEffect(() => {
     fetchCountries();
@@ -84,12 +87,7 @@ const AddressList = () => {
         },
       });
       if (res?.status == 200) {
-        console.log(res?.data);
         setAddressList(res?.data?.address);
-
-        // setHomeData(res?.data);
-        // const firstType = res?.data?.categories?.[0]?.name || '';
-        // setType(firstType);
       }
     } catch (error) {
       if (error.response) {
@@ -169,6 +167,109 @@ const AddressList = () => {
     }
   };
 
+  const updateAddress = async () => {
+    const data = await AsyncStorage.getItem('userDetail');
+    const userInfo = JSON.parse(data);
+    const token = userInfo?.token;
+
+    if (!country) {
+      showWarning('Please Slect the country');
+      return;
+    }
+
+    if (!city) {
+      showWarning('Please insert the City');
+      return;
+    }
+    if (!flat) {
+      showWarning('Please enter the flat number');
+      return;
+    }
+    if (!area) {
+      showWarning('Please enter the area');
+      return;
+    }
+    if (!pincode) {
+      showWarning('Please enter the pincode');
+      return;
+    }
+
+    const datas = {
+      user_id: userInfo?.user?.id,
+      country_name: country,
+      state_name: 'delhi',
+      city: city,
+      block: flat,
+      street: area,
+      zip_code: pincode,
+      latitude: 22.898989,
+      longitude: 33.98765678,
+      id: selectedAddress?.id,
+    };
+
+    const url = Constants.baseUrl10 + 'update-address';
+
+    try {
+      const res = await axios.post(url, datas, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res?.status == 200) {
+        console.log(res?.data);
+        showSucess(res?.data?.message);
+        setShowEditModal(false);
+        getAddress();
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log('Server Error:', error.response.data);
+        showWarning(error.response.data?.message);
+      } else if (error.request) {
+        console.log('No Response:', error.request);
+      } else {
+        console.log('Request Error:', error.message);
+      }
+    }
+  };
+
+  const onDelete = async () => {
+    const data = await AsyncStorage.getItem('userDetail');
+    const userInfo = JSON.parse(data);
+    const token = userInfo?.token;
+
+    const datas = {
+      id: selectedAddress?.id,
+    };
+
+    const url = Constants.baseUrl10 + 'delete-address';
+
+    try {
+      const res = await axios.post(url, datas, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (res?.status == 200) {
+        console.log(res?.data);
+        showSucess(res?.data?.message);
+        setShowDeleteModal(false);
+        getAddress();
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log('Server Error:', error.response.data);
+        showWarning(error.response.data?.message);
+      } else if (error.request) {
+        console.log('No Response:', error.request);
+      } else {
+        console.log('Request Error:', error.message);
+      }
+    }
+  };
+
   return (
     <View style={[styles.container, {paddingTop: inset.top}]}>
       <BackNavigationWithTitle
@@ -210,9 +311,13 @@ const AddressList = () => {
                   {item?.state_name} {item?.street} {item?.zip_code}
                 </Text>
               </View>
-              {/* <Pressable onPress={() => setShowActionModal(true)}>
+              <Pressable
+                onPress={() => {
+                  setSelectedAddress(item);
+                  setShowActionModal(true);
+                }}>
                 <Feather name="more-vertical" size={25} color={Colors.red} />
-              </Pressable> */}
+              </Pressable>
             </View>
           );
         }}
@@ -229,17 +334,29 @@ const AddressList = () => {
         showActionModal={showActionModal}
         onDelete={() => {
           setShowActionModal(false);
-          // setShowDeleteModal(true);
+          setShowDeleteModal(true);
         }}
-        onEdit={() => console.log('hurray!!!')}
+        onEdit={() => {
+          setShowActionModal(false);
+          setShowEditModal(true);
+          setCountry(selectedAddress?.country_name);
+          setCity(selectedAddress?.city);
+          setFlat(selectedAddress?.block);
+          setArea(selectedAddress?.street);
+          setPincode(selectedAddress?.zip_code);
+        }}
       />
 
       {showDeleteModal && (
         <DeleteModal
           onCancel={() => setShowDeleteModal(false)}
-          onDelete={() => setShowDeleteModal(false)}
+          onDelete={() => {
+            // setShowDeleteModal(false)
+            onDelete();
+          }}
         />
       )}
+      {/* add address */}
       <Modal
         animationIn="fadeInUp"
         animationInTiming={500}
@@ -281,20 +398,7 @@ const AddressList = () => {
                   }}
                 />
               )}
-              {/* <Dropdown
-                style={styles.dropdown}
-                placeholderStyle={styles.placeholderStyle}
-                selectedTextStyle={styles.selectedTextStyle}
-                itemTextStyle={styles.itemTextStyle}
-                data={countries}
-                maxHeight={250}
-                dropdownPosition={'auto'}
-                labelField="name"
-                valueField="id"
-                placeholder="State"
-                value={state}
-                onChange={item => setState(item?.id)}
-              /> */}
+
               <TextInput
                 value={city}
                 onChangeText={setCity}
@@ -323,17 +427,83 @@ const AddressList = () => {
                 placeholder="ZIP Code"
                 placeholderTextColor={Colors.gray10}
               />
-              {/* <View style={styles.currentLocationContainer}>
-              <FontAwesome6
-                name="location-crosshairs"
-                size={20}
-                color={Colors.black}
-              />
-              <Text style={styles.currentLocationText}>
-                Use My Current Location
-              </Text>
-            </View> */}
+
               <WineHuntButton text="Save" onPress={() => addAddress()} />
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </Modal>
+      {/* edit address */}
+      <Modal
+        animationIn="fadeInUp"
+        animationInTiming={500}
+        backdropOpacity={0.5}
+        animationOutTiming={500}
+        animationOut="fadeOutDown"
+        isVisible={showEditModal}
+        style={styles.modal}
+        onBackButtonPress={() => setShowEditModal(false)}
+        onBackdropPress={() => setShowEditModal(false)}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          style={styles.keyboardAvoidingView}>
+          <ScrollView
+            contentContainerStyle={styles.scrollContent}
+            bounces={false}>
+            <View style={[styles.modalContent, {paddingBottom: inset.bottom}]}>
+              <View style={styles.dragIndicator} />
+              <Text style={styles.title} allowFontScaling={false}>
+                Update Address
+              </Text>
+              {countries && countries.length > 0 && (
+                <Dropdown
+                  style={styles.dropdown}
+                  placeholderStyle={styles.placeholderStyle}
+                  selectedTextStyle={styles.selectedTextStyle}
+                  itemTextStyle={styles.itemTextStyle}
+                  data={countries}
+                  maxHeight={250}
+                  dropdownPosition={'auto'}
+                  labelField="name"
+                  valueField="id"
+                  placeholder="Country"
+                  value={country}
+                  onChange={item => {
+                    setCountry(item?.id);
+                    setCountryCode(item?.name);
+                  }}
+                />
+              )}
+
+              <TextInput
+                value={city}
+                onChangeText={setCity}
+                style={styles.input}
+                placeholder="City"
+                placeholderTextColor={Colors.gray10}
+              />
+              <TextInput
+                value={flat}
+                onChangeText={setFlat}
+                style={styles.input}
+                placeholder="Flat/Block"
+                placeholderTextColor={Colors.gray10}
+              />
+              <TextInput
+                value={area}
+                onChangeText={setArea}
+                style={styles.input}
+                placeholder="Apartment/Street/Area"
+                placeholderTextColor={Colors.gray10}
+              />
+              <TextInput
+                value={pincode}
+                onChangeText={setPincode}
+                style={styles.input}
+                placeholder="ZIP Code"
+                placeholderTextColor={Colors.gray10}
+              />
+              <WineHuntButton text="Update" onPress={() => updateAddress()} />
             </View>
           </ScrollView>
         </KeyboardAvoidingView>
